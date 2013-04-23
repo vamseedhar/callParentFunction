cf62ed3984e341fcafd9ad8288dbd53be7729c83
vvamseedhar <vamsee.vissu@gmail.com>
2013-04-23 15:07:42 +0530

    initial commit
    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<a href="javascript:void(0);" onclick="openchild()">click me</a>
+
+<script>
+    function openchild() {
+        //window.open('index1.html');
+        window.open('index1.html','name','height=200,width=150');
+    }
+    function makealert() {
+        alert('parent function activated!!!!!!!!!!!!!');
+    }
+    </script>
